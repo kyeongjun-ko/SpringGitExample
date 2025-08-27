87fa3581f0a1ebfb3639aebed0dedb5555fc2b42
kyeongjun <rhrudwnszoq@naver.com>
2025-08-27 11:15:00 +0900

    study: CI/CD

---
+++ src/main/java/com/example/myapp/config/WebMvcConfig.java
@@ -0,0 +1,30 @@
+package com.example.myapp.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.example.myapp.commons.HttpMethodOverrideFilter;
+
+import jakarta.servlet.Filter;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+	
+	@Bean
+	Filter shallowEtagHeaderFilter() {
+		return new ShallowEtagHeaderFilter();
+	}
+	
+	@Bean
+	FilterRegistrationBean<HttpMethodOverrideFilter> httpMethodOverrideFilter() {
+		FilterRegistrationBean<HttpMethodOverrideFilter> registrationBean = new FilterRegistrationBean<>();
+		
+		registrationBean.setFilter(new HttpMethodOverrideFilter());
+		registrationBean.addUrlPatterns("/api/*");
+		
+		return registrationBean;
+	}
+}
